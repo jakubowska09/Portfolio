3be29a4e0498ff3f77587f3dd9811be242bb1b74
jakubowska09 <mellepatrycia@gmail.com>
2018-11-27 12:21:29 +0100

    portfolio

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -1,2 +1,28 @@
+// button
+let graph = document.getElementById("graphisme")
+let ng = document.getElementById("ng")
+let site = document.getElementById("siteweb")
 
 
+let sg = document.getElementById("sectiongraphisme")
+let sd = document.getElementById("sectiondesign")
+
+function voir() {
+    let activee = document.querySelector(".d-none2");
+    sg.classList.add("d-block");
+    sd.classList.add("d-none2");
+    sd.classList.remove("d-block");
+    activee.classList.remove("d-none2");
+}
+graph.addEventListener("click", voir);
+
+
+function appli() {
+    let activee = document.querySelector(".d-none2");
+    sd.classList.add("d-block");
+    sg.classList.add("d-none");
+    sg.classList.remove("d-block");
+
+    // activee.classList.remove("d-none2")
+}
+ng.addEventListener("click", appli)
